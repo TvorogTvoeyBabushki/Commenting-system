df19aa529f43c2f1ebe6e1171cccd9db8bcdb448
foxta1ll <snake3773@mail.ru>
2023-06-29 21:34:30 +0300

    feat: добавил подсчет голосов у комментария

--- src/components/layout/comment/comment-item/commentItem.ts
+++ src/components/layout/comment/comment-item/commentItem.ts
@@ -1,11 +1,13 @@
 import { ICommentInfo } from '../comment-form/commentForm'
 
 import styles from './commentItem.module.scss'
+import Toolbar from './toolbar/toolbar'
 
 export class CommentItem {
 	wrapperDivElement: HTMLElement
 	imageElement: HTMLElement
 	infoDivElement: HTMLElement
+    toolbar: Toolbar
 
 	private _commentInfo: ICommentInfo | undefined
 
@@ -13,20 +15,21 @@ export class CommentItem {
 		this.wrapperDivElement = document.createElement('div')
 		this.imageElement = document.createElement('img')
 		this.infoDivElement = document.createElement('div')
+        this.toolbar = new Toolbar()
         
         if(localStorage.getItem('comment')){
             this._commentInfo = [...JSON.parse(<string>localStorage.getItem('comment'))].at(-1)
         }
 
         if(this._commentInfo) this.draw()
-        this.addStyle()
+        this.addStyle()        
 	}
 
     private addStyle() {
         this.wrapperDivElement.classList.add(styles.wrapper_comment)
     }
 
-	private draw() {
+    private draw() {
 		const imageProps = [
 			['src', `${this._commentInfo?.image}`],
 			['alt', 'user']
@@ -36,9 +39,8 @@ export class CommentItem {
 			this.imageElement.setAttribute(attr, val)
 		}
 
+        const wrapperInfoDivElement = document.createElement('div')
         const nameAndDateDivElement = document.createElement('div')
-        const toolbar = document.createElement('div')
-        const toolbarWrapper = document.createElement('div')
 
         for(const elem in this._commentInfo) {
             const paragraphElement = document.createElement('p')
@@ -46,47 +48,17 @@ export class CommentItem {
             if(elem === 'author' || elem === 'date') {
                 paragraphElement.innerText = `${this._commentInfo[elem]}`
                 nameAndDateDivElement.append(paragraphElement)
+                wrapperInfoDivElement.append(nameAndDateDivElement)
             }
 
             if(elem === 'comment') {
                 paragraphElement.innerText = `${this._commentInfo[elem]}`
 
-                toolbar.insertAdjacentElement('afterbegin', paragraphElement)
+                wrapperInfoDivElement.append(paragraphElement)
             }
-            
         }
-
-        const propsIconElement = [
-            [
-                ['src', '/public/answer-to-comment.png'],
-                ['alt', 'icon']
-            ],
-            [
-                ['src', '/public/empty-heart.png'],
-                ['alt', 'icon']
-            ]
-        ]
-
-        const [answer, favorite] = ['Ответить', 'В избранное']
-        
-        propsIconElement.forEach((item, index) =>{
-            const iconElement = document.createElement('img') 
-
-            for (const [attr, val] of item) {
-                iconElement.setAttribute(attr, val)
-            }
-            
-            const buttonElement = document.createElement('button')
-            const text = index === 0 ? answer : favorite            
-
-            buttonElement.append(iconElement, text)
-            toolbarWrapper.append(buttonElement)
-            toolbar.append(toolbarWrapper)
-        } ) 
-
     
-        this.infoDivElement.append(nameAndDateDivElement, toolbar)
-
+        this.infoDivElement.append(wrapperInfoDivElement, this.toolbar.draw())
 		this.wrapperDivElement.append(this.imageElement, this.infoDivElement)
 	}
 }
---
+++ src/components/layout/comment/comment-item/toolbar/toolbar.ts
@@ -0,0 +1,113 @@
+class ToolBar {
+	spanVoteCount: HTMLElement
+	private _voteCount: number
+
+	private _isDecrement: boolean
+	private _isIncrement: boolean
+
+	constructor() {
+		this.spanVoteCount = document.createElement('span')
+		this._voteCount = 0
+
+		this._isDecrement = true
+		this._isIncrement = true
+	}
+
+	private drawVoteCount() {
+		this.spanVoteCount.innerText = `${this._voteCount}`
+
+		return this.spanVoteCount
+	}
+
+	private decrement = (event: MouseEvent) => {
+		event.preventDefault()
+		const buttonElement = event.target as HTMLButtonElement
+
+		if (this._isDecrement && !this._isIncrement) {
+			this._voteCount--
+			this._isIncrement = true
+		} else if (this._isDecrement) {
+			this._voteCount--
+			this._isDecrement = false
+		} else {
+			buttonElement.disabled = true
+		}
+
+		buttonElement.disabled = false
+
+		this.drawVoteCount()
+	}
+
+	private increment = (event: MouseEvent) => {
+		event.preventDefault()
+		const buttonElement = event.target as HTMLButtonElement
+
+		if (!this._isDecrement && this._isIncrement) {
+			this._voteCount++
+			this._isDecrement = true
+		} else if (this._isIncrement) {
+			this._voteCount++
+			this._isIncrement = false
+		} else {
+			buttonElement.disabled = true
+		}
+
+		buttonElement.disabled = false
+
+		this.drawVoteCount()
+	}
+
+	public draw() {
+		const toolbar = document.createElement('div')
+		const toolbarWrapper = document.createElement('div')
+		const toolbarItemLeft = document.createElement('div')
+		const toolbarItemRight = document.createElement('div')
+
+		const propsIconElement = [
+			[
+				['src', '/public/answer-to-comment.png'],
+				['alt', 'icon']
+			],
+			[
+				['src', '/public/empty-heart.png'],
+				['alt', 'icon']
+			]
+		]
+		const [answer, favorite] = ['Ответить', 'В избранное']
+		const [decrement, increment] = ['-', '+']
+
+		propsIconElement.forEach((item, index) => {
+			const iconElement = document.createElement('img')
+
+			for (const [attr, val] of item) {
+				iconElement.setAttribute(attr, val)
+			}
+
+			const buttonElementLeft = document.createElement('button')
+			const buttonTextLeft = index === 0 ? answer : favorite
+
+			buttonElementLeft.append(iconElement, buttonTextLeft)
+			toolbarItemLeft.append(buttonElementLeft)
+
+			const buttonElementRight = document.createElement('button')
+			const buttonTextRight = index === 0 ? decrement : increment
+
+			index === 0
+				? buttonElementRight.addEventListener('click', this.decrement)
+				: buttonElementRight.addEventListener('click', this.increment)
+
+			buttonElementRight.append(buttonTextRight)
+			toolbarItemRight.append(buttonElementRight)
+
+			const previousButton = buttonElementRight.previousElementSibling
+			previousButton?.nextElementSibling?.before(this.drawVoteCount())
+
+			toolbarWrapper.append(toolbarItemLeft, toolbarItemRight)
+			toolbar.append(toolbarWrapper)
+		})
+
+		return toolbar
+	}
+}
+
+export default ToolBar
